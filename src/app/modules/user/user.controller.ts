7b502b77e78b2bf151e4b90daad1372e7322022c
MS Rahman <msrahman048@gmail.com>
2026-03-12 04:48:03 +0600

    Created User controller and route

---
+++ src/app/modules/user/user.controller.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from "express";
+import { User } from "./user.model";
+import httpStatus from "http-status-codes"
+
+const createUser = async(req: Request, res: Response) =>{
+    try {
+        const {name, email} = req.body;
+        const user = await User.create({
+            name,
+            email
+        })
+
+        res.status(httpStatus.CREATED).json({
+            message: "User Created Successfuly",
+            user
+        })
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    } catch (err: any) {
+        console.log(err);
+        res.status(httpStatus.BAD_REQUEST).json({
+            message: `Something Went Wrong!! ${err.message}`, err
+        })
+    }
+}
+
+export const UserControllers = {
+    createUser
+}
