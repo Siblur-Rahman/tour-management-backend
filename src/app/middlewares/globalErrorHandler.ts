3eaf68bf04e2cb9093952cbafb84944336ebf98c
MS Rahman <msrahman048@gmail.com>
2026-03-13 18:00:23 +0600

    Create Custom Error Class, AppErro

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,12 +1,23 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextFunction, Request, Response } from "express"
 import { envVars } from "../config/env"
+import AppError from "../errorHelpers/AppError"
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const globalErrorHandler = (err : any, req: Request, res: Response, next: NextFunction)=>{
 
-    const statusCode= 500
-    const message =`Something Went Wrong!! ${err.message}`
+    // eslint-disable-next-line prefer-const
+    let statusCode= 500
+    // eslint-disable-next-line prefer-const
+    let message =`Something Went Wrong!!`
+
+    if(err instanceof AppError){
+        statusCode = err.statusCode
+        message= err.message
+    } else if(err instanceof Error){
+        statusCode = 500;
+        message= err.message
+    }
     res.status(500).json({
         success: false,
          message,
--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -1,10 +1,11 @@
-import { Request, Response } from "express";
+import { NextFunction, Request, Response } from "express";
 import httpStatus from "http-status-codes"
 import { userServices } from "./user.service";
+import AppError from "../../errorHelpers/AppError";
 
-const createUser = async(req: Request, res: Response) =>{
+const createUser = async(req: Request, res: Response, next: NextFunction) =>{
     try {
-
+        throw new AppError(httpStatus.BAD_REQUEST, "fake error")
         const user = await userServices.createUser(req.body)
         res.status(httpStatus.CREATED).json({
             message: "User Created Successfuly",
@@ -13,9 +14,7 @@ const createUser = async(req: Request, res: Response) =>{
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     } catch (err: any) {
         console.log(err);
-        res.status(httpStatus.BAD_REQUEST).json({
-            message: `Something Went Wrong!! ${err.message}`, err
-        })
+        next(err)
     }
 }
 
