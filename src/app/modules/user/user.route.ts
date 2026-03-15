22a18f9237e68283f6fe670f52ff319ccfe23062
MS Rahman <msrahman048@gmail.com>
2026-03-15 19:57:09 +0600

    Verify Token and protect route using middleware

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,7 +1,11 @@
+import { NextFunction, Request, Response } from "express";
 import { Router } from "express";
 import { UserControllers } from "./user.controller";
-import { createUserZodSchema } from "./user.validation";
-import { validateRequest } from "../../middlewares/validateRequest";
+import jwt, { JwtPayload } from "jsonwebtoken";
+import AppError from "../../errorHelpers/AppError";
+import { Role } from "./user.interface";
+// import { createUserZodSchema } from "./user.validation";
+// import { validateRequest } from "../../middlewares/validateRequest";
 
 
 
@@ -9,9 +13,26 @@ import { validateRequest } from "../../middlewares/validateRequest";
 
 const router = Router()
 
-router.post("/register",
-    validateRequest(createUserZodSchema),
- UserControllers.createUser);
-router.get("/all-users", UserControllers.getAllUsers)
+router.post("/register", 
+    // validateRequest(createUserZodSchema), 
+    UserControllers.createUser);
+
+router.get("/all-users", async (req: Request, res: Response, next: NextFunction)=>{
+   try {
+     const accessToken = req.headers.authorization;
+     if(!accessToken){
+        throw new AppError(403, "No Token Recieved")
+     }
+     
+     const veryfiedToken = jwt.verify(accessToken, "secret")
+     
+     if((veryfiedToken as JwtPayload ).role !== Role.ADMIN){
+        throw new AppError(403, "You are not permited to view this route")
+     }
+    next()
+   } catch (err) {
+    next(err)
+   }
+}, UserControllers.getAllUsers)
 
 export const UserRoutes = router
