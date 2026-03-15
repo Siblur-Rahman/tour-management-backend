2d185cc13bd3f6abc3342ed4ff37dd0b1258ecd6
MS Rahman <msrahman048@gmail.com>
2026-03-15 14:31:29 +0600

    Fix sensitive password bugs, Password Hashing

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -2,10 +2,11 @@ import AppError from "../../errorHelpers/AppError";
 import { IAuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
 import httpStatus from "http-status-codes"
+import bcryptjs from "bcryptjs"
 
 
 const createUser= async(payload: Partial<IUser>)=>{
-        const {email, ...res} = payload;
+        const {email, password, ...res} = payload;
 
         const isUserExit = await User.findOne({email})
 
@@ -13,9 +14,14 @@ const createUser= async(payload: Partial<IUser>)=>{
             throw new AppError(httpStatus.BAD_REQUEST, "User Allready Exit")
         }
 
+        const hashedPassword = await bcryptjs.hash(password as string, 10)
+        
+       
         const authProvider : IAuthProvider = {provider: "credentials", providerId: email as string}
         const user = await User.create({
             email,
+            password: hashedPassword,
+            auths: [authProvider],
             ...res
         })
     return user
