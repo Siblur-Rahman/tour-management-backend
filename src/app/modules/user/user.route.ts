e75a5402d0f2293c0f7bb70701284e45b3023d85
MS Rahman <msrahman048@gmail.com>
2026-03-18 04:59:58 +0600

    Create Update User API and Password Re-Hashing

--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -2,8 +2,8 @@ import { Router } from "express";
 import { UserControllers } from "./user.controller";
 import { checkAuth } from "../../middlewares/checkAuth";
 import { Role } from "./user.interface";
-import { createUserZodSchema } from "./user.validation";
-import { validateRequest } from "../../middlewares/validateRequest";
+// import { createUserZodSchema } from "./user.validation";
+// import { validateRequest } from "../../middlewares/validateRequest";
 
 
 
@@ -14,12 +14,12 @@ const router = Router()
 
 
 router.post("/register", 
-    validateRequest(createUserZodSchema), 
+    // validateRequest(createUserZodSchema), 
     UserControllers.createUser);
 
-router.get("/all-users", 
+router.get("/all-users", checkAuth(Role.ADMIN, Role.SUPER_ADMIN), 
+UserControllers.getAllUsers);
 
-checkAuth(Role.ADMIN, Role.SUPER_ADMIN),
-UserControllers.getAllUsers)
+router.patch("/:id", checkAuth(...Object.values(Role)), UserControllers.updateUser);
 
 export const UserRoutes = router
